581a056fd3b2fd5c52a166e53ca480849d23bbd8
Raveesha-Induwara <raveeshe.abeysinghe@gmail.com>
2024-06-01 14:42:10 +0530

    Add Routes with react-router-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,25 @@
-import './App.css'
+import "./App.css";
+import Header from "./components/Header";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Chat from "./pages/Chat";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
 
 function App() {
-
   return (
-    <main>Hello World</main>
-  )
+    <main>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </main>
+  );
 }
 
-export default App
+export default App;
