1d096c369285c517fdbca7f38d7db64e028af172
Raveesha-Induwara <raveeshe.abeysinghe@gmail.com>
2024-06-02 12:25:22 +0530

    Setting up env variable in frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,7 +8,7 @@ import { AuthProvider } from "./context/AuthContext.tsx";
 import axios from "axios";
 import { Toaster } from "react-hot-toast";
 
-axios.defaults.baseURL = "http://localhost:5000/api/v1";
+axios.defaults.baseURL = import.meta.env.APP_API_BASE_URL;
 axios.defaults.withCredentials = true;
 
 const theme = createTheme({
